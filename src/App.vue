571e81a15b422ac0a0974a19c315676a97235610
Ryan Moulton <ryanmoulton116@gmail.com>
2020-06-22 18:30:29 -0700

    add basic functionality

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <QuoteMachine />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuoteMachine from "./components/QuoteMachine";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    QuoteMachine
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/QuoteMachine.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div class="conatiner">
+      <div class="row">
+        <div class="col-sm-6 offset-sm-3">
+          <div class="card mt-5">
+            <div class="card-body">
+              <div class="card-text">
+                <h2 class="quote">{{ quote }}</h2>
+                <p class="author">- Bob Ross</p>
+              </div>
+            </div>
+          </div>
+          <button @click="getQuote" class="btn btn-primary btn-lg">
+            New Quote
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import quotes from '../quotes';
+
+export default {
+  name: 'QuoteMachine',
+  data() {
+    return {
+      quote: quotes[0],
+    };
+  },
+  methods: {
+    getQuote() {
+      const quote = quotes[Math.floor(Math.random() * quotes.length)];
+      this.quote = quote;
+    },
+  },
+};
+</script>
+
+<style>
+button {
+  margin-left: 40%;
+}
+
+.card {
+  margin-bottom: 2rem;
+}
+
+.quote {
+  margin-bottom: 2rem;
+}
+
+.author {
+  font-size: 1.2rem;
+  text-align: right;
+}
+</style>
